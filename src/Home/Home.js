a8066139969770e3af142dd15d508df3295ae94d
Monique.Bass01 <monique.bass01@gmail.com>
2020-06-20 13:17:15 -0500

    auth works

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>INSIDE COMPONENT</h2>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/NewOuting/NewOuting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './NewOuting.scss';
+
+class NewOuting extends React.Component {
+  render() {
+    return (
+      <div className="NewOuting">
+        <h2>INSIDE COMPONENT</h2>
+
+      </div>
+    );
+  }
+}
+
+export default NewOuting;
