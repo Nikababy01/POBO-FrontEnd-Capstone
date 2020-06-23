17334b6e91445b3e3474a8385f3d3b47ef82c332
Monique.Bass01 <monique.bass01@gmail.com>
2020-06-22 19:53:16 -0500

    outing cards work

--- src/components/shared/OutingCard/OutingCard.js
+++ src/components/shared/OutingCard/OutingCard.js
@@ -7,8 +7,14 @@ class OutingCard extends React.Component {
   render() {
     const { outing } = this.props;
     return (
-      <div className= "OutingCard">
-        {outing.name}
+      <div className="OutingCard col-3">
+        <div className="card">
+            <img className="card-img-top" src={outing.imageUrl} alt="Outing Card"/>
+          <div className="card-body">
+            <h5 className="card-title">{outing.name}</h5>
+            <p className="card-text">{outing.description}</p>
+          </div>
+          </div>
       </div>
     );
   }
