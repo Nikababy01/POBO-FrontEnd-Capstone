7449e43d18ac88ff0de0a699c29ceb9b3511ca67
Monique.Bass01 <monique.bass01@gmail.com>
2020-06-27 12:18:00 -0500

    mvp branch

--- src/helpers/data/smash.js
+++ src/helpers/data/smash.js
@@ -1,37 +1,37 @@
-import outingsData from './outingsData';
-import commentsData from './commentsData';
-import parentsData from './parentsData';
+// import outingsData from './outingsData';
+// import commentsData from './commentsData';
+// import parentsData from './parentsData';
 
-const getParentComment = (outingId) => new Promise((resolve, reject) => {
-  outingsData.getSingleOuting(outingId)
-  .then((outingresponse) => {
-    parentsData.getParentByUid(uid)
-    .then((parentresponse) => {
-      commentsData.getCommentsByUid(uid)
-      .then((commentresponse) = {
-        outings = [];
-        outingresponse.forEach((outing) => {
-          const daOuting = {
-            id: outing.id,
-            uid: outing.uid,
-            name: outing.name,
-            description: outing.description,
-            imageUrl: outing.imageUrl,
-            address: outing.address,
-            city: outing.city,
-            state: outing.state,
-            zipcode: outing.zipcode, 
-            price:outing.price,
-            comment: commentresponse.filter((x) => x.id === outing.outingId),
-            parent: parentresponse.filter((x) => x.id === outing.uid)
-          };
-          outings.push(daOuting);
-        });
-        resolve(missions);
-      });
-    });
-  })
-  .catch((err) => reject(err));
-});
+// const getParentComment = (outingId) => new Promise((resolve, reject) => {
+//   outingsData.getSingleOuting(outingId)
+//   .then((outingresponse) => {
+//     parentsData.getParentByUid(uid)
+//     .then((parentresponse) => {
+//       commentsData.getCommentsByUid(uid)
+//       .then((commentresponse) = {
+//         outings = [];
+//         outingresponse.forEach((outing) => {
+//           const daOuting = {
+//             id: outing.id,
+//             uid: outing.uid,
+//             name: outing.name,
+//             description: outing.description,
+//             imageUrl: outing.imageUrl,
+//             address: outing.address,
+//             city: outing.city,
+//             state: outing.state,
+//             zipcode: outing.zipcode,
+//             price:outing.price,
+//             comment: commentresponse.filter((x) => x.id === outing.outingId),
+//             parent: parentresponse.filter((x) => x.id === outing.uid)
+//           };
+//           outings.push(daOuting);
+//         });
+//         resolve(missions);
+//       });
+//     });
+//   })
+//   .catch((err) => reject(err));
+// });
 
-export default getParentComment;
+// export default getParentComment;
